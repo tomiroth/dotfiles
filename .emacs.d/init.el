a63da681525dfd266c36cc18bce3863b4e991c45
Tom Elliott <tomelliott@C02XM0J3JGH6.local>
2021-06-14 09:58:07 +0100

    Added added rust support.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -233,6 +233,33 @@
   ;; (define-key php-mode-map (kbd "C-t c") 'phpunit-current-class)
   ;; (define-key php-mode-map (kbd "C-t p") 'phpunit-current-project))
 
+(use-package rustic
+    :ensure
+    :bind (:map rustic-mode-map
+                ("M-j" . lsp-ui-imenu)
+                ("M-?" . lsp-find-references)
+                ("C-c C-c l" . flycheck-list-errors)
+                ("C-c C-c a" . lsp-execute-code-action)
+                ("C-c C-c r" . lsp-rename)
+                ("C-c C-c q" . lsp-workspace-restart)
+                ("C-c C-c Q" . lsp-workspace-shutdown)
+                ("C-c C-c s" . lsp-rust-analyzer-status))
+    :config
+    ;; uncomment for less flashiness
+    ;; (setq lsp-eldoc-hook nil)
+    ;; (setq lsp-enable-symbol-highlighting nil)
+    ;; (setq lsp-signature-auto-activate nil)
+
+    ;; comment to disable rustfmt on save
+    (setq rustic-format-on-save t)
+    (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
+
+  (defun rk/rustic-mode-hook ()
+    ;; so that run C-c C-c C-r works without having to confirm
+    (setq-local buffer-save-without-query t))
+
+(use-package flycheck :ensure)
+
 (use-package company
   :after lsp-mode
   :hook
