91835dc8fd060080b3fde73640fc2208a9d2260c
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-20 20:31:24 +0100

    Added install file for mac.

---
+++ install/mac.sh
@@ -0,0 +1,24 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install zplug
+brew install zplug
+export ZPLUG_HOME=/opt/homebrew/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+#Instal git
+brew install git
+
+#Install NeoVim
+brew install neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Install alacritty
+brew install --cask alacritty
+
+#Install GNU stow
+brew install stow
