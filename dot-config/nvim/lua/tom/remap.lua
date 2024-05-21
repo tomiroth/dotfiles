786136e566f63194ab266b5635c742ad848f14bb
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-21 13:28:05 +0100

    Added some nvim key bindings for buffers.

--- dot-config/nvim/lua/tom/remap.lua
+++ dot-config/nvim/lua/tom/remap.lua
@@ -1,6 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pv", vim.cmd.Neogit)
+
+
+vim.keymap.set("n", "<leader>bj", vim.cmd.bn)
+vim.keymap.set("n", "<leader>bk", vim.cmd.bp)
+vim.keymap.set("n", "<leader>bx", vim.cmd.bd)
+
+
+-- Todo: Work out how to use the hyper key for binding.
 vim.keymap.set("n", "<Esc>[91;10u", vim.cmd.Neogit)
 
 
