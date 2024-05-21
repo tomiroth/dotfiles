6ea8cf3e3bbc97b5b3476fb4918a7abff2a25b50
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-21 12:41:45 +0100

    Added php to nvim lsp. Added some more lsp key binds.

--- dot-config/nvim/after/plugin/lsp.lua
+++ dot-config/nvim/after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local lsp = require('lsp-zero')
 
 require('mason').setup()
 require('mason-lspconfig').setup({
-  ensure_installed = { 'tsserver', 'rust_analyzer' }, -- Ensure tsserver is installed
+  ensure_installed = { 'tsserver', 'rust_analyzer', 'intelsense' }, 
   automatic_installation = true,     -- Automatically install language servers
   handlers = {
     function(server_name)
@@ -14,13 +14,20 @@ require('mason-lspconfig').setup({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr}
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
   vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+
+  vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "<leader>df", "<cmd>Telescope diagnostics<cr>", opts)
+
   vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
   vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
   vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+
   vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
 end)
