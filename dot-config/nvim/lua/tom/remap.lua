1d64c4113d779c81800beae23f535abc609a3d71
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-23 09:27:46 +0100

    Added some key mapping for yank/pasting to os clipboard.

--- dot-config/nvim/lua/tom/remap.lua
+++ dot-config/nvim/lua/tom/remap.lua
@@ -8,6 +8,14 @@ vim.keymap.set("n", "<leader>bj", vim.cmd.bn)
 vim.keymap.set("n", "<leader>bk", vim.cmd.bp)
 vim.keymap.set("n", "<leader>bx", vim.cmd.bd)
 
+-- Map <Leader>y to yank the current line to the system clipboard
+vim.api.nvim_set_keymap('n', '<Leader>y', '"+yy', { noremap = true, silent = true })
+-- Map <leader>y to yank current selecttion to system clipboard
+vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', { noremap = true, silent = true })
+-- Map <Leader>y to paste the current line to the system clipboard
+vim.api.nvim_set_keymap('n', '<Leader>Y', '"+p', { noremap = true, silent = true })
+-- Map <leader>y to paste current selecttion to system clipboard
+vim.api.nvim_set_keymap('v', '<Leader>Y', '"+p', { noremap = true, silent = true })
 
 -- Todo: Work out how to use the hyper key for binding.
 vim.keymap.set("n", "<Esc>[91;10u", vim.cmd.Neogit)
