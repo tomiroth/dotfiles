005220af96b261963b391489f9ae9c01854ff5f8
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-28 07:40:14 +0100

    Updated key bindings for system clipboard.

--- dot-config/nvim/lua/tom/remap.lua
+++ dot-config/nvim/lua/tom/remap.lua
@@ -4,13 +4,13 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Neogit)
 vim.keymap.set('n', '<leader>pd', "<cmd>lua require('tom.telescope').find_directories()<CR>")
 
 -- Map <Leader>y to yank the current line to the system clipboard
-vim.api.nvim_set_keymap('n', '<Leader>y', '"+yy', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>yy', '"+yy', { noremap = true, silent = true })
 -- Map <leader>y to yank current selecttion to system clipboard
-vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Leader>yy', '"+y', { noremap = true, silent = true })
 -- Map <Leader>y to paste the current line to the system clipboard
-vim.api.nvim_set_keymap('n', '<Leader>Y', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>yp', '"+p', { noremap = true, silent = true })
 -- Map <leader>y to paste current selecttion to system clipboard
-vim.api.nvim_set_keymap('v', '<Leader>Y', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Leader>yp', '"+p', { noremap = true, silent = true })
 
 -- Todo: Work out how to use the hyper key for binding.
 vim.keymap.set("n", "<Esc>[91;10u", vim.cmd.Neogit)
