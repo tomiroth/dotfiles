02b8ddbe3ab80ae316266e3c92b9d2ddb5167b4a
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-28 07:41:03 +0100

    Rust files are now formatted on save.

--- dot-config/nvim/lua/tom/spacing.lua
+++ dot-config/nvim/lua/tom/spacing.lua
@@ -29,3 +29,10 @@ augroup fmt
 augroup END
 ]])
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.rs",
+    callback = function()
+        vim.lsp.buf.format({ async = true })
+    end,
+})
+
