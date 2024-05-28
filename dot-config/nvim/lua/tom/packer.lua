4da6d103f826eec76436f448e49cf4dd58bd12ea
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-28 07:35:58 +0100

    updated hop keys.

--- dot-config/nvim/lua/tom/packer.lua
+++ dot-config/nvim/lua/tom/packer.lua
@@ -46,10 +46,6 @@ require('packer').startup(function(use)
 	use {
 	  'phaazon/hop.nvim',
 	  branch = 'v2', -- optional but strongly recommended
-	  config = function()
-	    -- you can configure Hop the way you like here; see :h hop-config
-	    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-	  end
 	}
 
 	use 'christoomey/vim-tmux-navigator'
