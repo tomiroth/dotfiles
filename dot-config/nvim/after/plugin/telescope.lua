606111b8e1b9a1941f27d3964f9b336f8472ecc7
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-28 14:31:08 +0100

    Added some key bindings to telescope.lua

--- dot-config/nvim/after/plugin/telescope.lua
+++ dot-config/nvim/after/plugin/telescope.lua
@@ -4,3 +4,20 @@ vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      n = {
+    	  ['<c-d>'] = require('telescope.actions').delete_buffer
+      }, -- n
+      i = {
+        ["<C-h>"] = "which_key",
+        ['<c-d>'] = require('telescope.actions').delete_buffer
+      } -- i
+    } -- mappings
+  }, -- defaults
+} -- telescope setup
