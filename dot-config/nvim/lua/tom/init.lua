a7303029d7b1e86fe462fd199b7d3ab4e6c432b3
Tom Elliott <tomelliott@X37L597LWV.local>
2024-05-30 10:27:02 +0100

    Added line numbers to nvim.

--- dot-config/nvim/lua/tom/init.lua
+++ dot-config/nvim/lua/tom/init.lua
@@ -3,3 +3,7 @@ require("tom.packer")
 require("tom.theme")
 require("tom.spacing")
 require("tom.telescope")
+
+-- Turn line numbers on and set them to relative
+vim.o.number = true
+vim.o.relativenumber = true
