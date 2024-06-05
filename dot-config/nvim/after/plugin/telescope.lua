6949c9e73f249242454cc95221690c7e88ba2758
Tom Elliott <tomelliott@X37L597LWV.local>
2024-06-05 12:20:17 +0100

    Nvim: Added new telescope keybindings

--- dot-config/nvim/after/plugin/telescope.lua
+++ dot-config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,10 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 
 
