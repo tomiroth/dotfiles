525b25ddf3d2ba9b5d7fe9b4aae4bd0dea311752
Tom Elliott <tomelliott@X37L597LWV.local>
2026-03-17 12:01:39 +0000

    nvim toggle prettier.

--- dot-config/nvim/lua/tom/spacing.lua
+++ dot-config/nvim/lua/tom/spacing.lua
@@ -21,13 +21,35 @@ vim.g['prettier#autoformat'] = 1
 vim.g['prettier#autoformat_config_present'] = 1
 
 
--- Autoformat with Prettier on save
-vim.cmd([[
-augroup fmt
-  au!
-  au BufWritePre *.js,*.jsx,*.ts,*.tsx,*.json,*.css,*.scss,*.md,*.html,*.yaml,*.yml silent! execute ':Prettier'
-augroup END
-]])
+-- state variable
+local prettier_enabled = true
+
+-- augroup for Prettier
+local fmt_group = vim.api.nvim_create_augroup("fmt", { clear = true })
+
+-- autocmd definition
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = fmt_group,
+  pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json", "*.css", "*.scss", "*.html", "*.yaml", "*.yml" },
+  callback = function()
+    if prettier_enabled then
+      vim.cmd("silent! Prettier")
+    end
+  end,
+})
+
+-- toggle function
+function TogglePrettier()
+  prettier_enabled = not prettier_enabled
+  if prettier_enabled then
+    print("✅ Prettier formatting enabled")
+  else
+    print("⛔ Prettier formatting disabled")
+  end
+end
+
+-- create a user command :TogglePrettier
+vim.api.nvim_create_user_command("TogglePrettier", TogglePrettier, {})
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*.rs",
